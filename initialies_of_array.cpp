dcbb04fbe73858d46552e884fc84cc1dc0934eb2
rohit jamre <152479611+rohit-jamre@users.noreply.github.com>
2024-03-12 16:10:02 +0530

    Array Initialisation
    problem statement of intialisation of Array

---
+++ initialies_of_array.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+	//intialization of array
+	int arr[5] = {2,4,6,8,5};
+	//accessing the array element
+	cout<<"array element is:"<<arr[4];
+}
