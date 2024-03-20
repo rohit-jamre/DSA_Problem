20dea2d57c4f5262d3e0959b9598ddf077d4ef0b
rohit jamre <152479611+rohit-jamre@users.noreply.github.com>
2024-03-20 01:47:08 -0700

    Add files via upload

---
+++ take_input_from_user_and_print_the_all_element.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main (){
+	cout<<"Enter the size of Array";
+	
+	int sizeOfArray;
+	int num [100];
+	cout<<"Enter the element of Array";
+	for(int i=0;i<sizeOfArray;i++){
+		cin>>num[i];
+		
+		}
+		cout<<"You entered the Array element are:";
+		for(int i=0;i<sizeOfArray;i++){
+	    cout<<" "<<num[i];
+			}
+			return 0;
+	
+}
