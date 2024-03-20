69e6d90b39b75fde698953d064ba000886ca3e9c
rohit jamre <152479611+rohit-jamre@users.noreply.github.com>
2024-03-20 01:43:29 -0700

    Add files via upload

---
+++ acces_array_all_element.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main (){
+	//declare  and intialies of array
+	int arr[5]= {4,6,8,9,4};
+	//accessing the all array element 
+	cout <<"array all  element is: ";
+	for (int i=0;i<4;i++){
+		cout<<" "<<arr[i];
+	}
+	return 0;
+}
